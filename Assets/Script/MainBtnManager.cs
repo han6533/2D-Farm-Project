ca969039c97e78324fdbc9b1c7934b9bf35a1725
= <hanhan6533@gmail.com>
2024-10-12 03:48:42 +0900

    Fixed

--- Assets/Script/MainBtnManager.cs
+++ Assets/Script/MainBtnManager.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainBtnManager : MonoBehaviour
 {
-    
+    public Text VersionText;
+    public void StartBtn()
+    {
+        VersionText.text = "v." + Application.version;
+        SceneManager.LoadScene("InGameScene");
+    }
+    public void ContiBtn()
+    {
+        SceneManager.LoadScene("InGameScene");
+    }
+
+    public Image SettingIMG;
+    public void SettingBtn()
+    {
+        if(!SettingIMG.gameObject.activeSelf)
+        {
+            SettingIMG.gameObject.SetActive(true);
+        }
+        else if(SettingIMG.gameObject.activeSelf)
+        {
+            SettingIMG.gameObject.SetActive(false);
+        }
+    }
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
 }
